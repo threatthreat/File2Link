b71f1a3bab7494185684cdeae1d0629cf5411ed0
threatthreat <grezy8007@gmail.com>
2025-02-08 21:48:48 -0800

    Update env.py

--- env.py
+++ env.py
@@ -5,17 +5,17 @@ from pyrogram import filters
 load_dotenv()
 
 # REQUIRED VARS
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DB_CHANNEL = os.getenv('DB_CHANNEL', "").strip()
-SUDO_IDS = list(map(int, os.getenv("SUDO_IDS", "").split()))
+API_ID = os.getenv("API_ID", "16708960").strip()
+API_HASH = os.getenv("API_HASH", "dda7630be99593256cb7c520eae0ce6d").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7769999187:AAElbFEeuIsOIm39vcFZ3oHTRY0KL-kYCwo").strip()
+DB_CHANNEL = os.getenv("-1002413856170").strip()
+SUDO_IDS = list(map(int, os.getenv("SUDO_IDS", "5482962500").split()))
 
 # OPTIONAL VARS
-MONGO_URL = os.getenv("MONGO_URL", "").strip()
-FORCE_SUB = os.getenv('FORCE_SUB', "").strip()
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://heisenberg:threat1234@cluster0.wkum1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").strip()
+FORCE_SUB = os.getenv('FORCE_SUB', "HeisenbergDx").strip()
 DELAY_TIME = float(os.getenv('DELAY_TIME', 0.5))
-HASH = int(os.getenv('HASH', 6969))
+HASH = int(os.getenv('HASH', 8080))
 
 
 SUDOERS = filters.user()
